84e1809f6afda752137eaa888929d59a55d8c64d
Ruben Diaz <rdiazbl54@gmail.com>
2019-02-10 00:39:26 +0000

    adding read file

--- server/controllers/ListController.js
+++ server/controllers/ListController.js
@@ -3,11 +3,13 @@ var fs = require('fs');
 module.exports = {
 
 	async readFile(req, res){
-
-		if(!fs.existsSync(req.param.path)) {
+		var {file} = req.body
+		file = ("/home/ubuntu/tts-flashcards/server/web-scraping/" + file)
+		console.log(file)
+		if(!fs.existsSync(file)) {
   			return res.status(404).send({filenotfound: req.param.path})
 		}
-		fs.readFile(req.param.path, 'utf8', function(err, contents){
+		fs.readFile(file , 'utf8', function(err, contents){
 			res.send(contents)
 		});
 	}
