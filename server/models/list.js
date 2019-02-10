488305732e424dbdbc3920fa7cd3a4456fa7ab9a
Ruben Diaz <rdiazbl54@gmail.com>
2019-02-10 01:29:03 +0000

    list fix

--- server/models/list.js
+++ server/models/list.js
@@ -1,9 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const List = sequelize.define('List', {
-    words: DataTypes.JSON,
+    words: DataTypes.TEXT,
     language: DataTypes.STRING,
-    nativeLanguage: DataTypes.STRING
+    nativeLanguage: DataTypes.STRING,
+    editable: DataTypes.BOOLEAN
   }, {});
   List.associate = function(models) {
     List.belongsTo(models.users)
