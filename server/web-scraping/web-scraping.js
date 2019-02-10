cd2e9cb0e025145e2e84114aa62ecedcff60aaa6
Ruben Diaz <rdiazbl54@gmail.com>
2019-02-10 03:02:31 -0800

    Web Scraping

--- server/web-scraping/web-scraping.js
+++ server/web-scraping/web-scraping.js
@@ -1,21 +1,73 @@
+// Languages Needed - Key Map Between Google Language Codes and Yandex Language Codes
+var languageObj = {
+	"AF":"af", // Afrikaans
+	"AR":"ar", // Arabic
+	"BG":"bg", // Bulgarian
+	"CA":"ca", // Catalan
+	"CMN":"zh", // Chinese ? WTF
+	"DA":"da", // Danish
+	"DE":"de", // German
+	"EL":"el", // Greek
+	"ES":"es", // Spanish
+	// "EN":"en", // English
+	"FA":"fa", // Persian / Farsi
+	"FI":"fi", // Finnish
+	// "FIL":"", // Philipino - NONE
+	"FR":"fr", // French
+	"GL":"gl", // Galacian
+	"HE":"he", // Hebrew
+	"HI":"hi", // Hindi
+	"HR":"hr", // Croatian
+	"HU":"hu", // Hungarian
+	"ID":"id", // Indonesian
+	"IS":"is", // Icelandic
+	"IT":"it", // Italian
+	"JA":"ja", // Japanese
+	"KO":"ko", // Korean
+	"LT":"lt", // Lithuanian
+	"MS":"ms", // Malaysian
+	"NB":"no", // Norwegian
+	"NL":"nl", // Dutch
+	"PL":"pl", // Polish
+	"PT":"pt", // Portuguese
+	"RO":"ro", // Romanian
+	"RS":"sr", // Serbian
+	"RU":"ru", // Russian
+	"SK":"sk", // Slovakian
+	"SL":"sl", // Slovenian
+	"SV":"sv", // Swedish
+	"TH":"th", // Thai
+	"UK":"uk", // Ukranian
+	"VI":"vi" // Vietnamese
+	// "ZU":"", // Zulu
+}
+
+// Loop through Languages and Init
+for (var prop in languageObj){
+	readFile(prop, languageObj[prop]);
+}
+
+	
+
 // Access Text Files
+function readFile(prop, key){
+	var xhttp = new XMLHttpRequest(),
+		url = 'http://18.188.201.66:8081/readFile',
+		fUrl = JSON.stringify({ 'file': `${prop}-EN-1000.csv` });
 
-var xhttp = new XMLHttpRequest(),
-	url = 'http://18.188.201.66:8081/readFile',
-	fUrl = JSON.stringify({ 'file': 'AF-EN-1000.csv' });
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+		  processRes(xhttp.responseText, prop, key);
+		}
+	};
 
-xhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	  processRes(xhttp.responseText);
-	}
+	xhttp.open("POST", url, true);
+	xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	xhttp.send(fUrl);
 };
 
-xhttp.open("POST", url, true);
-xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-xhttp.send(fUrl);
-
 // Split Into Array for API Send
-function processRes(res){
+function processRes(res, fromLang, toLang){
 	// console.log(res);
 	let tempArr,
 		langArr = [],
@@ -29,19 +81,17 @@ function processRes(res){
 	  	str += v+", ";
 	  }
 	});
-	console.log(langArr);
+	// console.log(langArr);
 	// console.log(str);
-	yandexTranslate(str, langArr);
+	yandexTranslate(str, langArr, fromLang, toLang);
 }
 
 // Send To Yandex For Translation
-function yandexTranslate(yandexText, langArr){
+function yandexTranslate(yandexText, langArr, fromLang, toLang){
 	// console.log("yandexText");
 	// console.log(yandexText);
 	console.log("--------------");
-	var fromLang = 'af',
-		toLang = 'es',
-		yandexUrl = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190114T000445Z.95291844b30dc809.79341b7169f080deb7cfa0ce4eb4a65e7897cf3a&text=${yandexText}&lang=${fromLang}-${toLang}`,
+	var yandexUrl = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190114T000445Z.95291844b30dc809.79341b7169f080deb7cfa0ce4eb4a65e7897cf3a&text=${yandexText}&lang=${fromLang}-${toLang}`,
 		xhr = new XMLHttpRequest();
 
 	xhr.onreadystatechange = function() {
@@ -49,7 +99,7 @@ function yandexTranslate(yandexText, langArr){
 			var resArr = JSON.parse(xhr.responseText),
 				resArr = resArr['text'];
 
-			translatedRes(resArr, langArr);
+			translatedRes(langArr, resArr, fromLang, toLang);
 		}
 	};
 
@@ -59,8 +109,10 @@ function yandexTranslate(yandexText, langArr){
 }
 
 // Join translated result into JSON
-function translatedRes(toArr, fromArr){
+function translatedRes(fromArr, toArr, fromLang, toLang){
 	let translatedObj = {
+		nativeLanguage: fromLang,
+		language: toLang,
 		words: []
 	};
 
@@ -77,10 +129,24 @@ function translatedRes(toArr, fromArr){
 		}
 
 		console.log(translatedObj);
-
+		// sendDB(translatedObj);
 	}
 }
 
+// Send translated result to DB
+function sendDB(json){
+	var xhr2 = new XMLHttpRequest(),
+		dbURL = '18.188.201.66:8081/addAdminList';
+
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(xhr.responseText);
+		}
+	};
+
+	xhr.open("POST", dbURL, true);
+	xhr.send(json);
+}
 
 
 // https://www.101languages.net/
